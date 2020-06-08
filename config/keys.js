d56aa8d3c8f9f38f7d444f380507283bdaeea230
mattorigami1 <matt.o@smallsparksoftware.co.uk>
2020-06-09 02:54:03 +0500

    mongodb path changed

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://Usama123:Usama123@ds141208.mlab.com:41208/videoserver",
-    secretOrKey: "secret" 
-  };
+  mongoURI: "mongodb://localhost/videoserver",
+  secretOrKey: "secret",
+};
