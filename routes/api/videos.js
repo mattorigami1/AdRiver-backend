9368b400f15884f0d49bef78658b8c358d8ebf73
mattorigami1 <matt.o@smallsparksoftware.co.uk>
2020-06-16 02:52:56 +0500

    Change in delete

--- routes/api/videos.js
+++ routes/api/videos.js
@@ -462,15 +462,6 @@ router.delete(
     //   })
     //   .catch((err) => makeResponse(res, 500, "Failed", err, true));
 
-    Video.deleteOne({ _id: req.params.id })
-      .then((vide) => {
-        return makeResponse(res, 200, "Deleted Video", video, false);
-      })
-      .catch((err) => {
-        return makeResponse(res, 500, "Failed", err, true);
-      });
-    return false;
-
     await Video.findOne({ _id: req.params.id })
       .then((video) => {
         s3.deleteObjects(
